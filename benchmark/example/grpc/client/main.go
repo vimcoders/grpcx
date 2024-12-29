4ad7115d575f894b086bdd20ddf67564b1f3ec8c
DESKTOP-RNVMINN\vim <vimcoders@gmail.com>
2024-12-29 21:24:35 +0800

    grpc

---
+++ benchmark/example/grpc/client/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpcx/benchmark/pb"
+	"io"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println(runtime.NumCPU())
+	runtime.GOMAXPROCS(3)
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := &Client{
+		ChatClient: pb.NewChatClient(conn),
+	}
+	for i := 0; i < 100; i++ {
+		go client.BenchmarkChat(context.Background())
+	}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-quit:
+			return
+		case <-ticker.C:
+			fmt.Println("NumCPU:", runtime.NumCPU(), "NumGoroutine:", runtime.NumGoroutine())
+		}
+	}
+}
+
+type Client struct {
+	pb.ChatClient
+	io.Closer
+}
+
+func (x *Client) BenchmarkChat(ctx context.Context) {
+	var b []byte
+	for i := 0; i < 100; i++ {
+		b = append(b, []byte("tokentoken")...)
+	}
+	message := string(b)
+	for {
+		if _, err := x.Chat(ctx, &pb.ChatRequest{Message: message}); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+}
---
+++ benchmark/example/grpc/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpcx/benchmark/pb"
+	"net"
+	"runtime"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println(runtime.NumCPU())
+	runtime.GOMAXPROCS(1)
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChatServer(s, MakeHandler())
+	if err := s.Serve(listener); err != nil {
+		panic(err)
+	}
+}
+
+type Handler struct {
+	sync.RWMutex
+	pb.ChatServer
+	total int64
+	unix  int64
+}
+
+// MakeHandler creates a Handler instance
+func MakeHandler() *Handler {
+	return &Handler{}
+}
+
+func (x *Handler) Chat(ctx context.Context, req *pb.ChatRequest) (*pb.ChatResponse, error) {
+	x.Lock()
+	defer x.Unlock()
+	x.total++
+	if x.unix != time.Now().Unix() {
+		fmt.Println(x.total, "request/s", "NumCPU:", runtime.NumCPU(), "NumGoroutine:", runtime.NumGoroutine())
+		x.total = 0
+		x.unix = time.Now().Unix()
+	}
+	return &pb.ChatResponse{}, nil
+}
